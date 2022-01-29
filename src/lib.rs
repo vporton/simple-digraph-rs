d23ef098b769a5f4870583e87491c8c2d7b8e67d
Victor Porton <porton.victor@gmail.com>
2022-01-29 15:08:18 +0200

    something

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,49 @@
-use std::{rc::Rc, ops::Deref};
+use std::rc::Rc;
 
+#[derive(Clone)]
 pub struct DigraphNode<T> {
     pub next: DigraphNodeRef<T>, // I made it `pub` to be able `item.next.next()` to remove an item from the middle.
     data: T,
 }
 
+impl<T> DigraphNode<T> {
+    fn new(next: DigraphNodeRef<T>, data: T) -> Self {
+        Self { next, data }
+    }
+}
+
 pub struct DigraphNodeRef<T> {
-    rc: Rc<DigraphNode<T>>,
+    rc: Option<Rc<DigraphNode<T>>>,
 }
 
 impl<T> DigraphNodeRef<T> {
-    pub fn from(rc: Rc<DigraphNode<T>>) -> Self {
+    pub fn new(value: DigraphNode<T>) -> Self {
+        Self::from(Some(Rc::new(value)))
+    }
+    pub fn from(rc: Option<Rc<DigraphNode<T>>>) -> Self {
         Self {
             rc
         }
     }
-    pub fn remove(&mut self) {
-        self.rc = self.rc.next.rc.clone()
+    pub fn remove(&mut self) -> bool {
+        if let Some(rc) = self.rc.clone() {
+            self.rc = rc.next.rc.clone();
+            true
+        } else {
+            false
+        }
+    }
+    pub fn prepend(&mut self, value: T) -> Self {
+        let new_node = DigraphNode::new(self.clone(), value);
+        let new_node_ref = DigraphNodeRef::new(new_node);
+        *self = new_node_ref.clone();
+        new_node_ref
+    }
+    #[allow(dead_code)]
+    fn node(&self) -> Option<DigraphNode<T>>
+        where T: Clone
+    {
+        self.rc.clone().map(|node| (*node).clone())
     }
 }
 
@@ -26,21 +53,13 @@ impl<T> Clone for DigraphNodeRef<T> {
     }
 }
 
-impl<T> Deref for DigraphNodeRef<T> {
-    type Target = DigraphNode<T>;
-
-    fn deref(&self) -> &Self::Target {
-        self.rc.deref()
-    }
-}
-
 impl<T> Iterator for DigraphNodeRef<T> {
-    type Item = DigraphNodeRef<T>;
+    type Item = Rc<DigraphNode<T>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if Rc::strong_count(&self.rc) != 0 { // FIXME
-            self.rc = (*self.rc).next.rc.clone();
-            Some(self.clone())
+        if let Some(rc) = self.rc.clone() {
+            self.rc = rc.next.rc.clone();
+            Some(rc.clone())
         } else {
             None
         }
@@ -55,11 +74,7 @@ impl<T: Clone> Iterator for DigraphNodeValuesIterator<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next) = self.underlying.next() {
-            Some(next.rc.data.clone())
-        } else {
-            None
-        }
+        self.underlying.next().map(|node| node.data.clone())
     }
 }
 
